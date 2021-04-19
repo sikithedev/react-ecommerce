af96c35343a92f1d5c58a300899e408b8f771b75
Muaz Sikiric <muaaz.sikiric@gmail.com>
2021-04-20 01:43:48 +0200

    divided homepage into smaller components and styled the menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import Homepage from './Homepage';
+import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import Directory from "../../components/directory/Directory";
+
+import "./homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
